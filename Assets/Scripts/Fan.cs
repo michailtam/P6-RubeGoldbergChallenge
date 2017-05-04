2cda93074aa8307045f3bc698b6689175b09ee39
Mike <mi.tamvak@gmail.com>
2017-05-05 01:43:01 +0300

    Add goal with particlesystem and fan behaviour

---
+++ Assets/Scripts/Fan.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour {
+
+  public float fanForce = 10f;
+  public float damping = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  private void OnTriggerEnter(Collider col) {
+    
+    // Check if the ball has entered the air flow of the fan
+    if(col.gameObject.CompareTag("Ball")) {
+      Debug.Log("BALL");
+      
+    }
+  }
+}
---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+  public ParticleSystem particle;
+
+  private void OnCollisionEnter(Collision col) {
+    if (col.gameObject.CompareTag("Ball")) {
+      // Create a particle system for 5 sec to indicate that the ball is in the goal
+      ParticleSystem p = Instantiate(particle, transform.position, Quaternion.Euler(-90,0,0));
+      Destroy(col.gameObject);    // Destroy the ball after entering the goal
+      Destroy(p, 3f);
+    }
+  }
+}
